66cefe8c7c7a53209861213e1cffc0dd505da40a
sryvprasad <sryvprasad@gmail.com>
2025-01-23 09:28:40 +1100

    removed the css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <GyroscopeDataViewer />
       </div>
       <div className="App">
-        <header className="App-header">
+        <header className="-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p> Open this website on your mobile device and give access and shake it to view the data.</p>
         </header>

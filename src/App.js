1a7f5f378a0d519bad9a6888f8e298729d06ec30
sryvprasad <sryvprasad@gmail.com>
2025-01-23 09:33:14 +1100

    css removerxd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div className="App">
         <GyroscopeDataViewer />
       </div>
-      <div className="App">
+      <div className="App1">
         <header className="-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p> Open this website on your mobile device and give access and shake it to view the data.</p>

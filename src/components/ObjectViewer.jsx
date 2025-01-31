3c6bd6cc28b6fcf69c58738fae22278e0b3340dc
sryvprasad <sryvprasad@gmail.com>
2025-01-31 15:00:18 +1100

    new per

--- src/components/ObjectViewer.jsx
+++ src/components/ObjectViewer.jsx
@@ -19,18 +19,32 @@ const ObjectViewer = () => {
       });
     };
 
-    // Check if the device supports DeviceOrientationEvent
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener("deviceorientation", handleOrientation);
-    } else {
-      console.error("Device orientation is not supported");
-    }
+    const requestPermission = async () => {
+      // For iOS 13+ devices, we need to request permission to access device orientation
+      if (typeof DeviceOrientationEvent.requestPermission === "function") {
+        try {
+          const permission = await DeviceOrientationEvent.requestPermission();
+          if (permission === "granted") {
+            // If permission is granted, start listening for orientation events
+            window.addEventListener("deviceorientation", handleOrientation);
+          } else {
+            console.error("Permission to access device orientation was denied.");
+          }
+        } catch (error) {
+          console.error("Error requesting permission:", error);
+        }
+      } else {
+        // For non-iOS devices, we can directly listen to the event
+        window.addEventListener("deviceorientation", handleOrientation);
+      }
+    };
+
+    // Request permission and start listening for orientation events
+    requestPermission();
 
     // Clean up the event listener when the component is unmounted
     return () => {
-      if (window.DeviceOrientationEvent) {
-        window.removeEventListener("deviceorientation", handleOrientation);
-      }
+      window.removeEventListener("deviceorientation", handleOrientation);
     };
   }, []);
 
@@ -39,26 +53,26 @@ const ObjectViewer = () => {
   const yPos = orientation.gamma / 90; // Mapping gamma (left/right) to y-axis
   const zPos = -orientation.alpha / 90; // Mapping alpha (compass) to z-axis
 
-  return (<>
-    <div >
-      <p>Orientation:</p>
-      <p>
-        X: {orientation?.alpha}, Y: {orientation?.beta}, Z: {orientation?.gamma}
-      </p>
-    </div>
-    <Canvas>
+  return (
+    <>
+      <div>
+        <p>Orientation Data:</p>
+        <p>
+          Alpha (Z-Axis): {orientation.alpha}, Beta (X-Axis): {orientation.beta}, Gamma (Y-Axis): {orientation.gamma}
+        </p>
+      </div>
 
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[0, 0, 5]} color="red" />
 
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[0, 0, 5]} color="red" />
-
-      {/* 3D object (cube) */}
-      <mesh position={[xPos, yPos, zPos]}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshPhongMaterial color="skyblue" />
-      </mesh>
-    </Canvas>
-  </>
+        {/* 3D object (cube) */}
+        <mesh position={[xPos, yPos, zPos]}>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshPhongMaterial color="skyblue" />
+        </mesh>
+      </Canvas>
+    </>
   );
 };
 
